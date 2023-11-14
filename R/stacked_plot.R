47497b722a79d98fa04f82c95ac7ebd57982ed0a
abedmarm <150075170+abedmarm@users.noreply.github.com>
2023-11-14 17:28:56 +0100

    Update and rename stacked_and_hyperbolic.R to stacked_plot.R

--- R/stacked_plot.R
+++ R/stacked_plot.R
@@ -28,28 +28,3 @@ ggplot(filtered_data, aes(x = Year, y = Number.of.people.without.access.to.elect
   theme(panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank()) +
   scale_x_continuous(breaks = seq(min(filtered_data$Year), max(filtered_data$Year), by = 2)) +
   scale_fill_viridis_d()
-
-
-
-#PLOT2
-library(dir2json)
-
-dat <- tibble::tribble(
-  ~level1, ~level2, ~level3, ~level4, ~value,
-  "Beverages", "Water", "", "", 20,
-  "Beverages", "Coffee", "", "", 15,
-  "Beverages", "Tea", "Black tea", "", 10,
-  "Beverages", "Tea", "White tea", "", 12,
-  "Beverages", "Tea", "Green tea", "Sencha", 8,
-  "Beverages", "Tea", "Green tea", "Gyokuro", 7,
-  "Beverages", "Tea", "Green tea", "Matcha", 5,
-  "Beverages", "Tea", "Green tea", "Pi Lo Chun", 6,
-  "Snacks", "Chips", "Potato Chips", "Classic", 18,
-  "Snacks", "Chips", "Potato Chips", "Barbecue", 14,
-  "Snacks", "Chips", "Tortilla Chips", "", 16,
-  "Snacks", "Chips", "Pita Chips", "", 10,
-  "Snacks", "Nuts", "Almonds", "", 22,
-  "Snacks", "Nuts", "Cashews", "", 17
-)
-shinyDataTree(dat, root = FALSE)
-
